7530d82c519269c876cc733c20bd9b033befe9d0
d1slike <eldustru@yandex.ru>
2016-02-22 00:29:03 +0300

    Удален неиспользуемый код из менеджера задач

--- src/ru/stankin/utils/TaskManager.java
+++ src/ru/stankin/utils/TaskManager.java
@@ -8,32 +8,13 @@ import java.util.concurrent.*;
 public class TaskManager {
     private final static TaskManager ourInstance = new TaskManager();
     private final ExecutorService executorService;
-    private final ScheduledExecutorService scheduledExecutorService;
 
     private TaskManager() {
         executorService = Executors.newFixedThreadPool(2, new TDeamonFactory("Executor_Thread_"));
-        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor(new TDeamonFactory("Scheduler_Thread_"));
     }
 
     public void execute(Executable executable) {
         executorService.execute(executable);
-    }
-
-    public ScheduledFuture<?> schedule(Executable executable, long delayInMillis) {
-        return scheduledExecutorService.schedule(executable, delayInMillis, TimeUnit.MILLISECONDS);
-    }
-
-    public ScheduledFuture scheduleAtFixedRate(Executable executable, long initialDelayInMillis, long delayInMillis) {
-        return scheduledExecutorService.scheduleAtFixedRate(executable, initialDelayInMillis, delayInMillis, TimeUnit.MILLISECONDS);
-    }
-
-    public void shutdown() {
-        try {
-            executorService.shutdown();
-            executorService.awaitTermination(1, TimeUnit.SECONDS);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
     }
 
     public static TaskManager getInstance() {
@@ -69,7 +50,6 @@ public class TaskManager {
             Thread thread = new Thread(r);
             thread.setDaemon(true);
             thread.setName(threadNames + threadNum++);
-            thread.setPriority(Thread.NORM_PRIORITY);
             return thread;
         }
     }
